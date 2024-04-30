b2ddb5467a7e21c2dd8e8bf0025bfdf3e8260aa6
mariasolegallina <gallinamariasole@gmail.com>
2024-04-30 21:48:56 +0200

    add cards

--- vite-boolfolio/src/App.vue
+++ vite-boolfolio/src/App.vue
@@ -1,25 +1,25 @@
 <script>
 // Import di axios
-import axios from 'axios';
+// import axios from 'axios';
 
 import ProjectIndex from './pages/projects/index.vue';
 
 export default {
   data() {
     return {
-      projects: [],
-      baseApiUrl: 'http://127.0.0.1:8000/api/'
+      // projects: [],
+      // baseApiUrl: 'http://127.0.0.1:8000/api/'
     }
   },
   mounted() {
-    axios.get(this.baseApiUrl + 'projects') 
-      .then(res => {
-        console.log(res);
-        this.projects = res.data.results;
-      })
-      .catch(error => {
-        console.error('Errore durante la richiesta:', error);
-      });
+    // axios.get(this.baseApiUrl + 'projects') 
+    //   .then(res => {
+    //     console.log(res);
+    //     this.projects = res.data.results;
+    //   })
+    //   .catch(error => {
+    //     console.error('Errore durante la richiesta:', error);
+    //   });
   },
 
   components: {
--- vite-boolfolio/src/pages/projects/index.vue
+++ vite-boolfolio/src/pages/projects/index.vue
@@ -1,29 +1,56 @@
 <template>   
     <div>
         <div class="container">
-            <h1>I miei progetti</h1>
+            <h1 class="my-3">I miei progetti</h1>
         </div>
 
         <div class="container">
-            <div class="grid">
-                <div class="card" style="width: 18rem;" v-for="project in projects" :key="project.id">
-                    <h5 class="card-title">{{ project.title }}</h5>
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col" v-for="project in projects" :key="project.id">
+                    <div class="card h-100">
+
+                        <!-- DA MODIFICARE: ora viene passato il type_id -->
+                        <div class="card-header" v-if="project.type_id">Categoria: {{ project.type_id }}</div>
+                        <div class="card-body">
+                            <h5 class="card-title">{{ project.title }}</h5>
+                            <p class="card-text">{{ project.description }}</p>
+                            <a href="#" class="btn btn-secondary">Go somewhere</a>
+                        </div>
+                    </div>
                 </div>
             </div>
+
+
+
+            
         </div>
     </div>
 </template>
   
 <script>
+import axios from 'axios';
+
 export default {
-    data() {
-        return {
-            projects: [],
-        }
-    },
-}  
+  data() {
+    return {
+      projects: [],
+      baseApiUrl: 'http://127.0.0.1:8000/api/'
+    }
+  },
+  mounted() {
+    axios.get(this.baseApiUrl + 'projects') 
+      .then(res => {
+        console.log(res);
+        this.projects = res.data.results;
+      })
+      .catch(error => {
+        console.error('Errore durante la richiesta:', error);
+      });
+  }
+}
 </script>
   
 <style scoped>
 
+    
 </style>
