36d02752522638b49fe2805173b189c97a487347
GiuseppeTrp <trapassogiuseppe158@gmail.com>
2024-04-30 13:19:53 +0200

    updated repo with vue, tested axios  work

---
+++ vite-boolfolio/src/App.vue
@@ -0,0 +1,40 @@
+<script>
+// axios import
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      // test
+      baseApiUrl: 'http://127.0.0.1.800/api/'
+      
+    }
+  },
+  mounted() {
+ axios.get('this.baseApiUrl + "/posts').then(res => {
+  console.log(res)
+ })
+    
+  },
+  methods: {
+
+    
+  },
+
+}
+</script>
+
+<template>
+  <div class="container-fluid py-5 d-flex flex-column justify-content-center align-items-center border">
+    <div class="container py-2 text-center">
+      <h1>ciao</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam tenetur repellat blanditiis, excepturi quod autem labore non vel, iure omnis odit, a nulla vitae error. Praesentium molestiae in fugiat autem!</p>
+
+    </div>
+   
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ vite-boolfolio/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+// modified in .scss
+import './style.scss'
+import App from './App.vue'
+// import bootstrap
+import * as bootstrap from 'bootstrap'
+createApp(App).mount('#app')
