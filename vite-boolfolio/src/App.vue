81c703c537d99b3a8886672ade3d0eee2eb5fd40
GiuseppeTrp <trapassogiuseppe158@gmail.com>
2024-04-30 17:35:46 +0200

    milestone 4 complete

--- vite-boolfolio/src/App.vue
+++ vite-boolfolio/src/App.vue
@@ -1,25 +1,24 @@
 <script>
-// axios import
+// Import di axios
 import axios from 'axios';
+
 export default {
   data() {
     return {
-      // test
-      baseApiUrl: 'http://127.0.0.1.800/api/'
-      
+      projects: [],
+      baseApiUrl: 'http://127.0.0.1:8000/api/'
     }
   },
   mounted() {
- axios.get('this.baseApiUrl + "/posts').then(res => {
-  console.log(res)
- })
-    
-  },
-  methods: {
-
-    
-  },
-
+    axios.get(this.baseApiUrl + 'projects') 
+      .then(res => {
+        console.log(res);
+        this.projects = res.data.results;
+      })
+      .catch(error => {
+        console.error('Errore durante la richiesta:', error);
+      });
+  }
 }
 </script>
 
@@ -29,9 +28,12 @@ export default {
       <h1>I topi non avevano nipoti</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, vel dolorum officia sed suscipit itaque obcaecati blanditiis. Hic deleniti consectetur minima placeat. Illum ratione ab assumenda laborum impedit nesciunt aliquid?</p>
     </div>
-   
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project.title }}
+      </li>
+    </ul>
   </div>
-  
 </template>
 
 <style scoped>
