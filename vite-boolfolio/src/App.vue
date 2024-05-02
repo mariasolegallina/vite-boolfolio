d01d2b5e5d620cf7617a3406c20c714d7582fda8
mariasolegallina <gallinamariasole@gmail.com>
2024-05-02 12:08:53 +0200

    code cleanup

--- vite-boolfolio/src/App.vue
+++ vite-boolfolio/src/App.vue
@@ -1,6 +1,4 @@
 <script>
-// Import di axios
-// import axios from 'axios';
 
 import AppHeader from './components/AppHeader.vue';
 
@@ -9,35 +7,25 @@ import AppHeader from './components/AppHeader.vue';
 export default {
   data() {
     return {
-      // projects: [],
-      // baseApiUrl: 'http://127.0.0.1:8000/api/'
+
     }
   },
   mounted() {
-    // axios.get(this.baseApiUrl + 'projects') 
-    //   .then(res => {
-    //     console.log(res);
-    //     this.projects = res.data.results;
-    //   })
-    //   .catch(error => {
-    //     console.error('Errore durante la richiesta:', error);
-    //   });
+
   },
 
   components: {
     AppHeader,
-    
-    
+      
   },
 }
 </script>
 
 <template>
-  <AppHeader />
-  <div class="container py-5 bg-dark"> 
-    <router-view></router-view>
+  <AppHeader></AppHeader>
+  
+  <router-view></router-view>
 
-  </div>
 </template>
 
 <style scoped>
--- vite-boolfolio/src/router.js
+++ vite-boolfolio/src/router.js
@@ -3,20 +3,20 @@ import { createRouter, createWebHistory } from 'vue-router';
 import index from './pages/projects/index.vue';
 import SingleProject from './pages/projects/SingleProject.vue'
 const router = createRouter({
-history: createWebHistory(),
-routes: [
-    //questo array  contiene tutte le rotte  dell' applicazione
-{
-path: '/',
-name: 'projects',
-component: index
-},
-{
-path: '/projects:id',
-name: 'single-project',
-component: SingleProject
-}
+    history: createWebHistory(),
+    routes: [
+        //questo array  contiene tutte le rotte  dell' applicazione
+        {
+            path: '/',
+            name: 'projects',
+            component: index
+        },
+        {
+            path: '/projects:id',
+            name: 'single-project',
+            component: SingleProject
+        }
 
-]
+    ]
 });
 export { router };
